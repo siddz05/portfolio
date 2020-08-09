d4395d9362d3c11cc7762ff094c63c913cb8b719
“im-siddharth” <siddharth@getmyparking.com>
2020-08-09 05:35:25 +0530

    Updates

---
+++ precache-manifest.9f7e04d6bff7e13342fe9b4cfb34a2fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2027952526f5b33750a12cfdd0c5ceac",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9d90ddc2a1fa7a99851b",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "00afcf0ee163f1fdaeef",
+    "url": "/portfolio/static/js/2.0e5a90ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.0e5a90ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d90ddc2a1fa7a99851b",
+    "url": "/portfolio/static/js/main.f15fa275.chunk.js"
+  },
+  {
+    "revision": "6564b422b6ca203d6011",
+    "url": "/portfolio/static/js/runtime-main.1e7eba3c.js"
+  },
+  {
+    "revision": "55e1797fd15a7113b2be5a8fc1363d93",
+    "url": "/portfolio/static/media/pic01.55e1797f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e011f31faae3dfc9e9e176e2ba581da9.js"
+  "/portfolio/precache-manifest.9f7e04d6bff7e13342fe9b4cfb34a2fe.js"
 );
 
 self.addEventListener('message', (event) => {
